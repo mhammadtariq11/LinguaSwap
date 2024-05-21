5f236d604ddb79c2bb955b98ae4e8090942f14a7
Muhammad Hammad Tariq <hammadtariq687@gmail.com>
2024-05-21 20:58:16 +0200

    Updated LinguaSwap

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 
+const headings = document.querySelectorAll('h1, h2, h3, h4, h5, h6');
+
+headings.forEach(heading => {
+  heading.addEventListener('mouseover', () => {
+    heading.style.transition = 'color 0.3s';
+    heading.style.color = '#9900d1';
+  });
+
+  heading.addEventListener('mouseout', () => {
+    heading.style.transition = 'color 0.3s';
+    heading.style.color = '';
+  });
+});
